023f981f78982bb62865aa4a428fb76ff2ea862b
Paul Laforgue <paul.laforgue@tezcore.com>
2019-10-22 18:18:25 +0200

    IO: protect write attempt in readonly mode

--- src/unix/index_unix.ml
+++ src/unix/index_unix.ml
@@ -294,13 +294,15 @@ module IO : Index.IO = struct
     mkdir (Filename.dirname file);
     match Sys.file_exists file with
     | false ->
-        let x = Unix.openfile file Unix.[ O_CREAT; O_CLOEXEC; mode ] 0o644 in
-        let raw = Raw.v x in
-        Raw.Offset.set raw 0L;
-        Raw.Fan.set_size raw fan_size;
-        Raw.Version.set raw;
-        Raw.Generation.set raw generation;
-        v ~fan_size ~offset:0L ~version:current_version raw
+        if readonly then Fmt.failwith "IO.v: cannot set a readonly file"
+        else
+          let x = Unix.openfile file Unix.[ O_CREAT; O_CLOEXEC; mode ] 0o644 in
+          let raw = Raw.v x in
+          Raw.Offset.set raw 0L;
+          Raw.Fan.set_size raw fan_size;
+          Raw.Version.set raw;
+          Raw.Generation.set raw generation;
+          v ~fan_size ~offset:0L ~version:current_version raw
     | true ->
         let x = Unix.openfile file Unix.[ O_EXCL; O_CLOEXEC; mode ] 0o644 in
         let raw = Raw.v x in
