e0b33b3f2e73eb30674a7d80bd38d01f31b0dc38
Gargi Sharma <gs051095@gmail.com>
2019-11-19 11:52:03 +0100

    Breaking up the big bench

--- bench/main.ml
+++ bench/main.ml
@@ -1,4 +1,5 @@
 open Common
+open Cmdliner
 
 let key_size = 32
 
@@ -43,17 +44,47 @@ let rec finds t count = function
       ignore (Index.find t k);
       finds t (count + 1) tl
 
-let () =
+let write_bench () =
   Fmt.epr "Adding %d bindings.\n%!" index_size;
   let rw = Index.v ~fresh:true ~log_size index_name in
-  let bindings, t1 = with_timer (fun () -> replaces rw [] index_size) in
-  Fmt.epr "\n%d bindings added in %fs.\n%!" index_size t1;
+  let _, t1 = with_timer (fun () -> replaces rw [] index_size) in
+  Fmt.epr "\n%d bindings added in %fs.\n%!" index_size t1
+
+let read_bench () =
   Fmt.epr "Finding %d bindings.\n%!" index_size;
+  let rw = Index.v ~fresh:true ~log_size index_name in
+  let bindings = replaces rw [] index_size in
   let (), t2 = with_timer (fun () -> finds rw 0 bindings) in
   Fmt.epr "\n%d bindings found in %fs (RW).\n%!" index_size t2;
-  Index.close rw;
+  Index.close rw
+
+let read_write_bench () =
+  let rw = Index.v ~fresh:true ~log_size index_name in
+  let bindings = replaces rw [] index_size in
   let ro = Index.v ~readonly:true ~log_size index_name in
   let (), t3 = with_timer (fun () -> finds ro 0 bindings) in
   Index.close ro;
-  Fmt.epr "\n%d bindings found in %fs (RO).\n%!" index_size t3;
-  print_newline ()
+  Fmt.epr "\n%d bindings found in %fs (RO).\n%!" index_size t3
+
+exception UnsupportedCommand of string
+
+let run input =
+  match input with
+  | "write" -> write_bench ()
+  | "read" -> read_bench ()
+  | "read_write" -> read_write_bench ()
+  | "all" ->
+      write_bench ();
+      read_bench ();
+      read_write_bench ()
+  | _ -> raise (UnsupportedCommand "input not supported")
+
+let input =
+  let doc = "options: write, read, read_write; all" in
+  Arg.(value & opt string "" & info [ "b"; "bench" ] ~doc)
+
+let cmd =
+  let doc = "Specify the benchmark you want to run." in
+  (Term.(const run $ input), Term.info "run" ~doc ~exits:Term.default_exits)
+
+let () = Term.(exit @@ eval cmd)
