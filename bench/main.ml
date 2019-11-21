bd4afabde60de676388ac460cd00894adf85f090
Thomas Gazagnaire <thomas@gazagnaire.org>
2019-11-21 09:59:32 +0100

    benchmarks: refactor the benchmark split

--- bench/main.ml
+++ bench/main.ml
@@ -44,44 +44,51 @@ let rec finds t count = function
       ignore (Index.find t k);
       finds t (count + 1) tl
 
-let write_bench () =
+let run input =
   Fmt.epr "Adding %d bindings.\n%!" index_size;
   let rw = Index.v ~fresh:true ~log_size index_name in
-  let _, t1 = with_timer (fun () -> replaces rw [] index_size) in
-  Fmt.epr "\n%d bindings added in %fs.\n%!" index_size t1
-
-let read_bench () =
+  let bindings =
+    match input with
+    | `Write | `All ->
+        let bindings, t1 = with_timer (fun () -> replaces rw [] index_size) in
+        Fmt.epr "\n%d bindings added in %fs.\n%!" index_size t1;
+        bindings
+    | _ -> replaces rw [] index_size
+  in
   Fmt.epr "Finding %d bindings.\n%!" index_size;
-  let rw = Index.v ~fresh:true ~log_size index_name in
-  let bindings = replaces rw [] index_size in
-  let (), t2 = with_timer (fun () -> finds rw 0 bindings) in
-  Fmt.epr "\n%d bindings found in %fs (RW).\n%!" index_size t2;
+  let () =
+    match input with
+    | `Find `RW | `All ->
+        let (), t2 = with_timer (fun () -> finds rw 0 bindings) in
+        Fmt.epr "\n%d bindings found in %fs (RW).\n%!" index_size t2
+    | _ -> ()
+  in
+  let () =
+    match input with
+    | `Find `RW | `All ->
+        let ro = Index.v ~readonly:true ~log_size index_name in
+        let (), t3 = with_timer (fun () -> finds ro 0 bindings) in
+        Index.close ro;
+        Fmt.epr "\n%d bindings found in %fs (RO).\n%!" index_size t3
+    | _ -> ()
+  in
   Index.close rw
 
-let read_write_bench () =
-  let rw = Index.v ~fresh:true ~log_size index_name in
-  let bindings = replaces rw [] index_size in
-  let ro = Index.v ~readonly:true ~log_size index_name in
-  let (), t3 = with_timer (fun () -> finds ro 0 bindings) in
-  Index.close ro;
-  Fmt.epr "\n%d bindings found in %fs (RO).\n%!" index_size t3
-
-exception UnsupportedCommand of string
-
-let run input =
-  match input with
-  | "write" -> write_bench ()
-  | "read" -> read_bench ()
-  | "read_write" -> read_write_bench ()
-  | "all" ->
-      write_bench ();
-      read_bench ();
-      read_write_bench ()
-  | _ -> raise (UnsupportedCommand "input not supported")
-
 let input =
-  let doc = "options: write, read, read_write; all" in
-  Arg.(value & opt string "" & info [ "b"; "bench" ] ~doc)
+  let doc =
+    "Select which benchmark(s) to run. Available options are: `write`, \
+     `find-rw`, `find-ro` or `all`."
+  in
+  let options =
+    Arg.enum
+      [
+        ("all", `All);
+        ("write", `Write);
+        ("find-rw", `Find `RW);
+        ("find-ro", `Find `RO);
+      ]
+  in
+  Arg.(value & opt options `All & info [ "b"; "bench" ] ~doc)
 
 let cmd =
   let doc = "Specify the benchmark you want to run." in
