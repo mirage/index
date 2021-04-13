362a61d6b68e560c99c34d0e8087091b866c8a99
mattiasdrp <5543639+mattiasdrp@users.noreply.github.com>
2021-04-13 10:45:15 +0200

    small update

--- src/io_intf.ml
+++ src/io_intf.ml
@@ -33,12 +33,9 @@ module type S = sig
 
   val append_batch : t -> ((string -> unit) -> unit) -> unit
   (** [append_batch io f] will automatically call [flush io] when a certain
-      limit of offset has been reached. Unlike [append], this allows more
-      control over the moment in which [flush] may be called since it allows to
-      provide all the consumer functions in one batch.
-
-      This function definitely replaces [append] since [append] could flush
-      incomplete data.Str
+      limit of offset has been reached. This allows more control over the moment
+      in which [flush] may be called since it allows to provide all the consumer
+      functions in one batch.
 
       Helper: [f] is a function containing a [string] in its closure and waiting
       for a [string -> unit] function to handle this string. *)
