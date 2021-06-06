mirage/index

commit 42b0af7410006d3a30dce6526a9e7437e53297f6

Fix a race between merge and sync
These two process are calling IO.Header.get and IO.Header.set concurrently
on the log file.

- At the end of the merge, the entries in log_async are copied into log.
  It starts by calling IO.Header.set(log) with a new generation number,
  copies all the entries and then clear log_async.
- On the other hand, the sync operation starts by calling IO.Header.get(log)
  and then is looking at entries in log_async. This can lead to dataloss, as
  we might read an old version of [log] and then an empty [async_log].

To avoid this, just ensure that sync_log_async is called before
IO.Header.get(log).

src/index.ml
@@ -238,13 +238,15 @@ struct
     if min = None then Tbl.clear log.mem;
     iter_io ?min add_log_entry log.io
 
-  let sync_log_async ?(generation_change = false) t =
+  let sync_log_async t =
     match t.log_async with
     | None -> t.log_async <- try_load_log t (log_async_path t.root)
     | Some log ->
         let offset = IO.offset log.io in
-        let new_offset = IO.force_offset log.io in
-        if generation_change || offset <> new_offset then sync_log_entries log
+        let h = IO.Header.get log.io in
+        if t.generation <> h.generation then sync_log_entries log
+        else if offset < h.offset then sync_log_entries ~min:offset log
+        else if offset > h.offset then assert false
 
   let sync_index ~generation t =
     may (fun (i : index) -> IO.close i.io) t.index;
@@ -264,14 +266,26 @@ struct
     (match t.log with
     | None -> t.log <- try_load_log t (log_path t.root)
     | Some _ -> ());
+    (* there is a race between sync and merge:
+
+       - At the end of the merge, the entries in log_async are copied
+       into log. [merge] starts by calling IO.Header.set(log) with a
+       new generation number, copies all the entries and then clear
+       log_async.
+
+       - so here we need to make sure we do the same thing in reverse:
+       start by syncing [log_async], then read [log]'s headers. At
+       worse, [log_async] and [log] might contain duplicated entries,
+       but we won't miss any. These entries will be added to [log.mem]
+       using Tbl.replace where they will be deduplicated. *)
+    sync_log_async t;
     match t.log with
-    | None -> sync_log_async t
+    | None -> ()
     | Some log ->
         let log_offset = IO.offset log.io in
         hook `Before_offset_read;
         let h = IO.Header.get log.io in
         hook `After_offset_read;
-        sync_log_async ~generation_change:(t.generation <> h.generation) t;
         if t.generation <> h.generation then (
           Log.debug (fun l ->
               l "[%s] generation has changed, reading log and index from disk"
@@ -609,7 +623,7 @@ struct
                 log_async.mem;
               IO.flush log.io;
               t.log_async <- None);
-          IO.clear ~generation log_async.io;
+          IO.clear ~generation:(Int64.succ generation) log_async.io;
           IO.close log_async.io;
           hook `After;
           Mutex.unlock t.merge_lock;
@@ -671,7 +685,7 @@ struct
       Mutex.with_lock t.rename_lock (fun () ->
           let log =
             match t.log_async with
-            | Some async_log -> async_log
+            | Some log -> log
             | None -> assert_and_get t.log
           in
           append_key_value log.io key value;

commit 516b69fba7f791f8324a53cc2653d3805f2342bf

Attempt to recover from a broken IO header invariant
In the event that the `log_async` headers change unexpectedly when
attempting a sync, we can attempt a recovery by reloading the entire
`log` from scratch (as this may contain new updates).

In principle, this case should never occur, but the previous `assert
false` has been triggered via Tezos at least once
(https://gitlab.com/tezos/tezos/-/issues/1403), and the failing node was
able to recover on restart.

C.f. 42b0af7410006d3a30dce6526a9e7437e53297f6, which introduced this
assertion failure.

Signed-off-by: Craig Ferguson <me@craigfe.io>

src/index.ml
@@ -241,13 +241,14 @@ struct
         hook `Reload_log_async;
         t.log_async <- try_load_log t (Layout.log_async ~root:t.root)
     | Some log ->
-        let offset = IO.offset log.io in
+        let old_generation = t.generation in
+        let old_offset = IO.offset log.io in
         let h = IO.Header.get log.io in
         if
           (* the generation has changed *)
-          h.generation > Int63.succ t.generation
+          h.generation > Int63.succ old_generation
           || (* the last sync was done between clear(log) and clear(log_async) *)
-          (h.generation = Int63.succ t.generation && h.offset = Int63.zero)
+          (h.generation = Int63.succ old_generation && h.offset = Int63.zero)
         then (
           (* close the file .*)
           IO.close log.io;
@@ -255,10 +256,21 @@ struct
           hook `Reload_log_async;
           t.log_async <- try_load_log t (Layout.log_async ~root:t.root)
           (* else if the disk offset is greater, reload the newest data. *))
-        else if offset < h.offset then sync_log_entries ~min:offset log
+        else if old_offset < h.offset then sync_log_entries ~min:old_offset log
           (* else if the offset is lesser, that means the [log_async] was
              cleared, and the generation should have changed. *)
-        else if offset > h.offset then assert false
+        else if old_offset > h.offset then (
+          (* Should never occur, but we can recover by reloading the log from
+             scratch rather than just hard failing. *)
+          Log.err (fun l ->
+              l
+                "[%s] log_async IO header monotonicity violated during sync:@,\
+                \  offset: %a -> %a@,\
+                \  generation: %a -> %a@,\
+                 Reloading the log to compensate." (Filename.basename t.root)
+                Int63.pp old_offset Int63.pp h.offset Int63.pp old_generation
+                Int63.pp h.generation);
+          sync_log_entries log)
 
   (** Syncs the [index] of the instance by checking on-disk changes. *)
   let sync_index t =